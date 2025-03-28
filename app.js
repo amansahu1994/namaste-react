e65c487825806d5d6bd94a0638a01850b6ecd360
amansahu1994 <as9806910153@gmail.com>
2025-03-29 00:52:25 +0530

    "episode-02"

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
-    {id:"parent", key:"1"},
+    { id: "parent", key: "1" },
     [React.createElement(
         "div",
-        {id: "child1", key:2},
-        [React.createElement("h1",{key:4}, "I'm an h1 tag"), React.createElement("h2",{key:5}, "I'm an h2 tag")]
+        { id: "child1", key: 2 },
+        [React.createElement("h1", { key: 4 }, "I'm an h1 tag"), React.createElement("h2", { key: 5 }, "I'm an h2 tag")]
     ),
     React.createElement(
         "div",
-        {id: "child2", key:21},
-        [React.createElement("h1",{key:4}, "I'm an h1 tag"), React.createElement("h2",{key:5}, "I'm an h2 tag")]
+        { id: "child2", key: 21 },
+        [React.createElement("h1", { key: 4 }, "I'm an h1 tag"), React.createElement("h2", { key: 5 }, "I'm an h2 tag")]
     )
     ]
 )
