5590e4bb22565a611610b122c084d4d4ac13da43
amansahu1994 <as9806910153@gmail.com>
2025-04-01 23:46:51 +0530

    "episode-03"

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent", key: "1" },
-    [React.createElement(
-        "div",
-        { id: "child1", key: 2 },
-        [React.createElement("h1", { key: 4 }, "I'm an h1 tag"), React.createElement("h2", { key: 5 }, "I'm an h2 tag")]
-    ),
-    React.createElement(
-        "div",
-        { id: "child2", key: 21 },
-        [React.createElement("h1", { key: 4 }, "I'm an h1 tag"), React.createElement("h2", { key: 5 }, "I'm an h2 tag")]
-    )
-    ]
-)
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from App.js!");
-console.log(parent)
+// JSX => Babel transpiles it to React.createElement => JS Object => HTMLElement(When Render)
+// React Element
+const Title = (
+    <h1 className="head">
+        Namaste React using JSX
+        </h1>
+        );
+
+const number = 12345;
+
+// React Functional Component
+const HeadingComponent1 = () => {
+    return <h1>This is a Functional Component1</h1>;
+}
+
+// Component Composition => Put one component inside another one
+const HeadingComponent2 = () => (
+    <div id="container">
+        {/* <Title /> */}
+        <h2>{Title}</h2>
+        <h1 className="heading">This is a Functional Component2</h1>
+    </div>
+    );
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent)
+root.render(<HeadingComponent2/>);
